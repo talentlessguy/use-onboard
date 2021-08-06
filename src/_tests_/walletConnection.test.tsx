bff92c1e6a487ba8ef04c4fb64c824805ffe5ca2
Clish254 <clishilla@gmail.com>
2021-08-06 17:00:26 +0300

    test: isWalletSelected should be false by default

--- src/_tests_/walletConnection.test.tsx
+++ src/_tests_/walletConnection.test.tsx
@@ -2,8 +2,10 @@ import { render, screen } from '@testing-library/react'
 import { NoAppId } from './components/NoAppId'
 import '@testing-library/jest-dom/extend-expect'
 
+// eslint-disable-next-line no-undef
 test('isWalletSelected should be false by default', () => {
   render(<NoAppId />)
   const isWalletSelected = screen.getByText(/^walletSelected:/)
+  // eslint-disable-next-line no-undef
   expect(isWalletSelected).toHaveTextContent(`walletSelected: Connect`)
 })
