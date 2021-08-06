d6ea849d3a9e3a032ef29d86e455d624e1ec6859
Clish254 <clishilla@gmail.com>
2021-08-07 00:25:26 +0300

    test: use ganache's ethereum provider

--- src/_tests_/walletConnection.test.tsx
+++ src/_tests_/walletConnection.test.tsx
@@ -1,11 +1,22 @@
-import { render, screen } from '@testing-library/react'
-import { NoAppId } from './components/NoAppId'
+import { render } from '@testing-library/react'
+import { useOnboard } from '../index.ts'
+import Ganache from 'ganache-cli'
 import '@testing-library/jest-dom/extend-expect'
+
+function setup() {
+  const returnVal = {}
+  function TestComponent() {
+    Ganache.provider()
+    Object.assign(returnVal, useOnboard())
+    return null
+  }
+  render(<TestComponent />)
+  return returnVal
+}
 
 // eslint-disable-next-line no-undef
 test('isWalletSelected should be false by default', () => {
-  render(<NoAppId />)
-  const isWalletSelected = screen.getByText(/^walletSelected:/)
+  const defaultReturnedValues = setup()
   // eslint-disable-next-line no-undef
-  expect(isWalletSelected).toHaveTextContent(`walletSelected: Connect`)
+  expect(defaultReturnedValues.isWalletSelected).toBe(false)
 })
